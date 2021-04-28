c4e94cbbd033726dfada73f6ffeb40379a52e634
inwardik <volodymyr.syrotkin@gmail.com>
2021-04-28 09:40:53 +0300

    Longest Substring Without Repeating Characters

--- Longest Substring Without Repeating Characters/sol.py
+++ Longest Substring Without Repeating Characters/sol.py
@@ -1,13 +1,38 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if not str:
+        if not s:
             return 0
-        if len(str) == 1:
+        if len(s) == 1:
             return 1
-        
 
-        return 1
+        def is_substr_valid(substr):
+            if len(substr) == len(set(substr)):
+                return True
+            return False
+
+        window = 1
+        for i in range(len(s)):
+            if len(s) < i + window + 1:
+                break
+            while len(s) >= i + window + 1 and is_substr_valid(s[i:i+window+1]):
+                window += 1
+
+        return window
+
+    def lengthOfLongestSubstring2(self, s: str) -> int:
+        if len(s) == 0:
+            return 0
+        max_length = 0
+        start = 0
+        lastIdx = dict()
+        for i, c in enumerate(s):
+            if c in lastIdx and lastIdx[c]>=start:
+                max_length = max(max_length, i - start)
+                start = lastIdx[c] + 1
+            lastIdx[c] = i
+        
+        return max(max_length, len(s) - start)
 
 s = Solution()
-res = s.lengthOfLongestSubstring('abraca')
+res = s.lengthOfLongestSubstring('au')
 print(res)
--- Longest Substring Without Repeating Characters/test_sol.py
+++ Longest Substring Without Repeating Characters/test_sol.py
@@ -8,7 +8,10 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(s.lengthOfLongestSubstring("abcabcbb"), 3)
         self.assertEqual(s.lengthOfLongestSubstring("bbbbb"), 1)
         self.assertEqual(s.lengthOfLongestSubstring("pwwkew"), 3)
+        self.assertEqual(s.lengthOfLongestSubstring("au"), 2)
+        self.assertEqual(s.lengthOfLongestSubstring("a"), 1)
         self.assertEqual(s.lengthOfLongestSubstring(""), 0)
+        self.assertEqual(s.lengthOfLongestSubstring("abcdefghabcdefghijklmnopqrstuvwxyzabcdaefghijklmnopqrstuvwxyz"), 26)
 
 if __name__ == '__main__':
     unittest.main()
